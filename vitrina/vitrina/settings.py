42deb8faea476617c3c9e4fe905558f91c879773
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2025-05-26 19:50:59 -0300

    Update Database

--- vitrina/vitrina/settings.py
+++ vitrina/vitrina/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'vitrina.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME" : "vitrina",
+        "USER" : "root",
+        "PASSWORD" : "1234",
+        "HOST" : "127.0.0.1",
+        "PORT" : "3306",
+
     }
 }
 
